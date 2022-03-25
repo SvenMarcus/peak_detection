43cafe16145fb54a965a97a3f36df5fc3aec14ec
ELGarulli <elisa.garulli@charite.de>
2022-03-25 18:15:01 +0100

    restructured still to implement local linear fitting

--- peak_detection.py
+++ peak_detection.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from utils import average_distance, compare_functions
+from utils import average_distance, get_left_bound, get_right_bound
 from scipy.signal import find_peaks, peak_prominences
 
 x = np.genfromtxt('data_L.csv', delimiter=',')
@@ -8,19 +8,16 @@ x = x[1:]
 max_x, _ = find_peaks(x, prominence=1)
 avg_distance = abs(int(average_distance(max_x)))
 
-compare_functions(x[0:avg_distance], x[max_x[0]])
-peaks_expansion = []
-peaks_f = []
-
+plt.plot(x)
 for p in max_x:
     l = p - avg_distance if p - avg_distance > 0 else 0
     r = p + avg_distance if p + avg_distance < len(x) else len(x)
-    slice = x[l:r]
-    peak = np.asarray([p-l])
-    prom = peak_prominences(slice, peak)
-    plt.vlines(prom[1]+l, ymin=0, ymax=2.5, color="green")
-    plt.vlines(prom[2]+l, ymin=0, ymax=2.5, color="red")
+    rb = get_right_bound(x[l:r], x[max_x[0]], p - l)[-1] + l
+    lb = get_left_bound(x[l:r], x[max_x[0]], p - l)[0] + l
+    plt.vlines(rb, ymin=0, ymax=2.5, color="green")
+    plt.vlines(lb, ymin=0, ymax=2.5, color="red")
 
-plt.plot(x)
-plt.vlines(max_x, ymin=0, ymax=2.5)
+
+plt.vlines(max_x, ymin=0, ymax=2.5, color="blue")
+
 plt.show()
--- utils.py
+++ utils.py
@@ -10,51 +10,41 @@ def average_distance(a):
     return np.median(distances)
 
 
-# interpolate slice
-# get f linear
-# min(f_lin - f_interpol)
-# intersection f linear, f interpol
-
-def get_boundaries(y, peaks, ):
-    # y = mx + peak
-    x = np.arange(0, len(y))
-    f = interpolate.interp1d(x, y)
-
-    return
-
-
-def compare_functions(array, peak):
-    array = array[:35]
-    m = np.arange(0, 5, 0.1)
+def get_left_bound(array, peak, p_idx):
+    array = array[:p_idx]
+    m = np.arange(0, 1, 0.01)
     x = np.arange(0, len(array), 1)
-    y_lin = lambda m_, c, x_: m_ * x_ + c
-    y_lin_up = lambda m_, c, l, x_: m_ * x_ +c-m_*l
     y_inter = interpolate.interp1d(x, array)
+    y_lin = lambda m_, c_, l_, x_: m_ * x_ + c_ - m_ * l_
+    m_best = fit_line(m=m, c=peak, l=len(x), x=x, y_inter=y_inter, y_lin=y_lin)
+    y_diff = np.diff(np.sign(y_lin(m_best, peak, len(x), x) - y_inter(x)))
+    idx_intersection = np.argwhere(y_diff).flatten()
+    return idx_intersection
 
+
+def get_right_bound(array, peak, p_idx):
+    array = array[p_idx:]
+    m = np.arange(-0.5, 0., 0.01)
+    x = np.arange(0, len(array), 1)
+    y_inter = interpolate.interp1d(x, array)
+    y_lin = lambda m_, c_, l_, x_: m_ * x_ + c_ - m_ * l_
+    m_best = fit_line(m=m, c=peak, l=0, x=x, y_inter=y_inter, y_lin=y_lin)
+    y_diff = np.diff(np.sign(y_lin(m_best, peak, 0, x) - y_inter(x)))
+    idx_intersection = np.argwhere(y_diff).flatten()
+    return idx_intersection
+
+
+def fit_line(m, c, l, x, y_inter, y_lin):
     cumsum = []
-
     for m_i in m:
-        y_diff = abs(np.subtract(y_lin_up(m_i, peak, len(x), x), y_inter(x)))
+        y_diff = abs(np.subtract(y_lin(m_i, c, l, x), y_inter(x)))
         cumsum_i = np.cumsum(y_diff)[-1]
         cumsum.append(cumsum_i)
-        plt.plot(y_diff)
-        plt.plot(y_lin_up(m_i, peak, len(x), x))
-        plt.plot(y_inter(x))
-        plt.show()
+        #plt.plot(y_diff)
+        #plt.plot(y_lin(m_i, c, l, x))
+        #plt.plot(y_inter(x))
+        #plt.show()
 
     val, idx = min((val, idx) for (idx, val) in enumerate(cumsum))
     m_best = m[idx]
-    y_diff = np.abs(np.subtract(y_lin(m_best, peak, x), y_inter(x)))
-    plt.plot(y_diff)
-    plt.plot(y_lin(m_best, peak, x))
-    plt.plot(y_inter(x))
-    plt.show()
-
-    # slope = m[np.argmin(cumsum)[0]]
-    print(m_best)
-    return
-
-
-
-def fit_line():
-    """"""
+    return m_best
