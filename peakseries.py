622b0c0a56e34aa62871feaebba90271c095ffa6
Sven Marcus <s.marcus@outlook.de>
2022-05-18 11:47:54 +0200

    Trying to clean up intersection finding

--- peakseries.py
+++ peakseries.py
@@ -1,15 +1,17 @@
 from dataclasses import dataclass
-from typing import List, Tuple
+from typing import List, Tuple, TypeVar, Union
 
 import numpy as np
 import numpy.typing as npt
+from scipy import interpolate
 from scipy.signal import find_peaks
 
-from utils import get_left_right_bound, median_distance
-
+from consts import INTERSECTION_NUMBER, LOCAL_POINTS, SLOPE_INITIAL_TILT, TILTING_STEPS
+from utils import get_inversion_idx, get_local_slope, median_distance
 
 ValueArray = npt.NDArray[np.float64]
 PositionArray = npt.NDArray[np.int64]
+GenericArrayType = TypeVar("GenericArrayType", ValueArray, PositionArray)
 
 PeakBound = Tuple[int, int]
 
@@ -58,10 +60,67 @@ def _collect_accurate_bounds(
 def _accurate_bounds_for_peak(
     gradients: ValueArray, peak_bound_estimate: PeakBound, peak_position: int
 ) -> PeakBound:
-    bounds = get_left_right_bound(
-        y=gradients[peak_bound_estimate[0] : peak_bound_estimate[1]],
-        px=peak_position,
+    return _get_left_right_bound(
+        gradients=gradients[peak_bound_estimate[0] : peak_bound_estimate[1]],
+        peak_position=peak_position,
         left_crop=peak_bound_estimate[0],
     )
 
-    return bounds
+
+def _get_left_right_bound(
+    gradients: ValueArray, peak_position: float, left_crop: int
+) -> PeakBound:
+    """
+    Computes the left and right bounds of a peak, analysing the first derivative.
+    First it interpolates the signal to be able to use it as a function.
+    Then fixes a line through the point corresponding to the max of the peak in the
+    original signal. The line is initialized close to a slope computed locally around the point.
+    Finally it's rotated contra clock wise, as long as it has three intersection points with
+    the interpolated functions.
+    :param y: first derivative of the signal
+    :param px: x of the maxima point
+    :param left_crop: left cropping of the signal (for shifting)
+    :return: left and right intersection
+    """
+    x: PositionArray = np.arange(0, len(gradients), 1, dtype=np.int64)
+    cropped_peak_position = peak_position - left_crop
+    intersections = _find_relevant_intersections(x, gradients, cropped_peak_position)
+    print(len(intersections))
+    return (intersections[0] + left_crop, intersections[-1] + left_crop)
+
+
+def _get_slope_tilts(
+    x: PositionArray, gradients: ValueArray, peak_position: float
+) -> ValueArray:
+    slope = get_local_slope(peak_position, x, gradients, LOCAL_POINTS)
+    return np.linspace(slope + SLOPE_INITIAL_TILT, 0, TILTING_STEPS, endpoint=False)
+
+
+def _find_relevant_intersections(
+    x: PositionArray, gradients: ValueArray, peak_position: float
+) -> List[int]:
+    slope_tilts = _get_slope_tilts(x, gradients, peak_position)
+    peak_gradient = gradients[peak_position]
+    print(peak_gradient)
+    for current_slope in slope_tilts:
+        y_intercept = _get_y_intercept(peak_position, peak_gradient, current_slope)
+        function = _linear_function(x, current_slope, y_intercept)
+        intersections = _intersections_of_functions(gradients, function)
+
+        if len(intersections) != INTERSECTION_NUMBER:
+            break
+    return intersections
+
+
+def _get_y_intercept(x: float, y: float, slope: float) -> float:
+    return y - (slope * x)
+
+
+def _intersections_of_functions(
+    gradients: GenericArrayType, current_function: GenericArrayType
+) -> List[int]:
+    return get_inversion_idx(current_function - gradients)
+
+
+def _linear_function(x: GenericArrayType, slope: float, y_intercept: float) -> ValueArray:
+    return x * slope + y_intercept  # type: ignore
--- utils.py
+++ utils.py
@@ -3,7 +3,6 @@ from typing import Tuple, List
 from numpy import ndarray
 from scipy import interpolate
 from scipy.stats import linregress
-from consts import (LOCAL_POINTS, SLOPE_INITIAL_TILT, TILTING_STEPS, INTERSECTION_NUMBER)
 
 
 def median_distance(a: ndarray) -> ndarray:
@@ -16,43 +15,6 @@ def median_distance(a: ndarray) -> ndarray:
     for i in range(len(a) - 1):
         distances.append(a[i] - a[i + 1])
     return abs(np.median(distances))
-
-
-def get_left_right_bound(y: ndarray, px: float, left_crop: int) -> Tuple[int, int]:
-    """
-    Computes the left and right bounds of a peak, analysing the first derivative.
-    First it interpolates the signal to be able to use it as a function.
-    Then fixes a line through the point corresponding to the max of the peak in the
-    original signal. The line is initialized close to a slope computed locally around the point.
-    Finally it's rotated contra clock wise, as long as it has three intersection points with
-    the interpolated functions.
-    :param y: first derivative of the signal
-    :param px: x of the maxima point
-    :param left_crop: left cropping of the signal (for shifting)
-    :return: left and right intersection
-    """
-    x = np.arange(0, len(y), 1, dtype=float)
-    y_inter = interpolate.interp1d(x, y)
-
-    px_i = px - left_crop
-    py = y[px_i]
-    slope = get_local_slope(px_i, x, y, LOCAL_POINTS)
-
-    m = np.linspace(slope + SLOPE_INITIAL_TILT, 0, TILTING_STEPS, endpoint=False)
-    current_slope = slope
-    for mi in m:
-        c = py - (mi * px_i)
-        line = lambda x_: mi * x_ + c
-        intersections = get_inversion_idx(line(x) - y_inter(x))
-        if len(intersections) == INTERSECTION_NUMBER:
-            current_slope = mi
-        else:
-            break
-
-    final_line = lambda x_: current_slope * x_ + c
-    intersections = get_inversion_idx(final_line(x) - y_inter(x))
-    inter = (intersections[0] + left_crop, intersections[-1] + left_crop)
-    return inter
 
 
 def get_inversion_idx(array: ndarray) -> List[int]:
@@ -83,7 +45,7 @@ def get_local_slope(px_i: float, x: ndarray, y: ndarray, neighbor_points: int) -
     :return: the slope
     """
 
-    p_neigh_x = x[px_i - neighbor_points:px_i + neighbor_points]
-    p_neigh_y = y[px_i - neighbor_points:px_i + neighbor_points]
+    p_neigh_x = x[px_i - neighbor_points : px_i + neighbor_points]
+    p_neigh_y = y[px_i - neighbor_points : px_i + neighbor_points]
     slope, intercept, r, p, se = linregress(p_neigh_x, p_neigh_y)
     return slope
